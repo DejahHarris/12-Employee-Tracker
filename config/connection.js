f6b092c0af3c603e732a152c5cb6330217c2356f
Dejah Harris <dejahharris@Dejahs-MBP.attlocal.net>
2022-12-15 09:01:22 -0500

    update repo

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-const mysql = require('mysql');
+const mysql = require('mysql2')
 
-const con=mysql.createConnection({
+var connection=mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",

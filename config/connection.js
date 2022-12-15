eb56cc49a27eb7df543f731f7aea87c7a5a760ac
Dejah Harris <dejahharris@Dejahs-MBP.attlocal.net>
2022-12-14 23:12:29 -0500

    update repo

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: "localhost",
-    // port: 3306,
+    port: 3306,
     user: "root",
     password: "",
-    // socketPath: "mysql-socket-path",
     database: "workDB"
 });
 
